5acb7b1b5405e1d3f6ad0ae79899cdd7102e35df
Sneha Savani <savanisneha@gmail.com>
2021-02-22 19:22:34 -0700

    Implement search movies for upcoming and now playing movies

--- bookmyshow/src/components/UpcomingMoviesList.js
+++ bookmyshow/src/components/UpcomingMoviesList.js
@@ -5,7 +5,8 @@ class UpcomingMoviesList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: ["1"]
+            movies: [], 
+            query: ''   
         }
     }
 
@@ -16,13 +17,27 @@ class UpcomingMoviesList extends Component {
     getList = () => {
         fetch('/api/upcomingMovies')
             .then(res => res.json())
-            .then(list => this.setState({ list: list.moviesList}))
+            .then(list => this.setState({ movies: list.moviesList}))
+    }
+
+    handleChange(e) {
+        this.setState({ query: e.target.value });
+    }
+
+    handleClick(query, e){
+        fetch('/api/movies/search/' + query)
+            .then(res => res.json())
+            .then(list => this.setState({ movies: list.moviesList.results}))
     }
 
     render() {
         return (
             <div style={{ padding: 20 }}>
-                <MovieMediaCard list={this.state.list} />
+                <input type="text" value={this.props.query} onChange={ this.handleChange.bind(this) } placeholder="Search by title"></input>
+                <button onClick={ this.handleClick.bind(this, this.state.query)}>
+                    Search 
+                </button>
+                <MovieMediaCard list={this.state.movies} />
             </div>
         );
     }
