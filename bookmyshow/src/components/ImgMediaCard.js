123666cd79fbb0ffca7fa9d08590bc899a76f80e
snehasavani93 <savanisneha@gmail.com>
2021-04-28 18:04:22 -0700

    Refactor movie details page

---
+++ bookmyshow/src/components/ImgMediaCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { Avatar } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(12),
+    height: theme.spacing(12),
+  }
+}));
+
+
+export default function ImgMediaCard(props) {
+ const classes = useStyles();
+  const imageUrl = "https://image.tmdb.org/t/p/w500//";
+  return (
+    <div>
+      <GridList cols={5}>
+        {props.castDetails.map((tile) => (
+          <GridListTile key={tile.id}>
+            <Avatar alt={tile.name} src={imageUrl+ tile.profile_path} className={classes.large}  />
+              <p>{tile.name}</p>
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
--- bookmyshow/src/components/MoviesDetail.js
+++ bookmyshow/src/components/MoviesDetail.js
@@ -1,60 +1,95 @@
 import React, { Component } from 'react';
 import ReactPlayer from "react-player";
+import ImgMediaCard from './ImgMediaCard';
+import '../../src/Pagination.scss';
 
 class MoviesDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            detail: {1: "x"},
-            video: "1"
+            detail: { 1: "x" },
+            video: "1",
+            movieCast: [{ 1: "x" }]
         }
     }
 
     componentDidMount() {
         this.getDetails();
         this.getVideos();
+        this.getCast();
     }
 
     getDetails = () => {
-        const movieId  = this.props.match.params.id;
+        const movieId = this.props.match.params.id;
         fetch(`/api/movie/${movieId}`)
             .then(res => res.json())
-            .then(result => this.setState({ detail: result.moviesDetail}))
+            .then(result => this.setState({ detail: result.moviesDetail }))
     }
 
     getVideos = () => {
-        const movieId  = this.props.match.params.id;
+        const movieId = this.props.match.params.id;
         fetch(`/api/movie/${movieId}/videos`)
             .then(res => res.json())
-            .then(result => this.setState({ video: result.moviesDetail.results}))
+            .then(result => this.setState({ video: result.moviesDetail.results }))
+    }
+
+    getCast = () => {
+        const movieId = this.props.match.params.id;
+        fetch(`/api/movie/${movieId}/credits`)
+            .then(res => res.json())
+            .then(result => this.setState({ movieCast: result.moviesDetail.cast }))
     }
 
     render() {
         const data = this.state.detail;
-        const videoId = this.state.video[0].key;
-        const videoUrl = "https://www.youtube.com/watch?v=" + videoId;
+        const moviesCast = this.state.movieCast.filter(cast => cast.known_for_department === "Acting" && cast.order < 10)
+        const videoId = this.state.video[0] ? this.state.video[0].key : null;
+        const videoUrl = videoId != null ? "https://www.youtube.com/watch?v=" + videoId : null;
         const posterImageUrl = "https://image.tmdb.org/t/p/w500//" + data.poster_path;
         return (
             <div>
-            <div className="position: absolute; top: 0; left: 0; padding-top: 50px">
-            <ReactPlayer
-                url={videoUrl} 
-                width='100%'
-                height='100%'
-            />
+                <div id="player1">
+                    {videoUrl ? (<ReactPlayer
+                        className="player"
+                        url={videoUrl}
+                        height="50%"
+                        width="100%"
+                    />) : <h1></h1>}
+                
+                <div className="row col-md-12 col-sm-12">
+                    <div className="col-md-6 col-sm-6" style={{ paddingLeft: "130px", paddingTop: "240px" }}>
+                        <img src={posterImageUrl} alt="" width="200px" height="200px" />
+                    </div>
+                    <div className="col-md-6 col-sm-6" style={{ paddingTop: "260px", marginLeft: "-88px" }}>
+                            <h1>{data.title}</h1>
+                            {/* <p>{data.original_language}</p>   */}
+                    </div>
+
+
+                    <div className="row col-md-12 col-sm-12" style={{marginTop: "-60px", marginLeft: "20px"}}>
+                        <div className="col-md-4 col-sm-4">
+                        </div>
+                        <div className="col-md-6 col-sm-6">
+                            <strong>Summary</strong>
+                            <p>{data.overview}</p>
+                            {/* Tagline: {data.tagline} <br /> */}
+                            {/* <strong>Release Date:</strong> {data.release_date} <br /> */}
+                        </div>
+                    </div>
+
+
+                    <div className="col-md-3 col-sm-3" style={{marginLeft: "438px"}}>
+                        <strong>Cast</strong>
+                    </div>
+                    <div className="container mb-3" style={{marginLeft: "438px", marginTop: "15px"}}>
+                        <div className="row d-flex flex-row py-3">
+                            <ImgMediaCard castDetails={moviesCast} />
+                        </div>
+                    </div>
+                </div>
+                </div>
             </div>
-            <div className="col-md-6 col-sm-6">
-            <img src={posterImageUrl} alt="" width="200px" height="200px" />
-            </div>
-            <div className=" padding-left:50px col-md-6 col-sm-6">
-            <h2>{data.title}</h2>
-            </div>
-            
-            Summary: {data.overview} <br/>
-            Original Language: {data.original_language} <br/>
-            Tagline: {data.tagline} <br/>
-            Release Date: {data.release_date} <br/>
-            </div>
+
         );
     }
 }
